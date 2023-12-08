340924173046a67f53ea3fe4e9b93d9ef228241d
Davi <davi.cintra2002@gmail.com>
2023-12-08 00:15:46 -0300

    feat: Refactor FileFlexClient constructor and add FileFlexClient interface

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 /**
  * @fileoverview This file is the entry point of the FileFlex SDK.
- * @description It exports all the public classes and functions of the SDK. 
+ * @description It exports all the public classes and functions of the SDK.
  */
 
-export class FileFlexClient {
-  constructor() {
-    console.log('FileFlexClient constructor');
+export default class FileFlexClient {
+  private AWS_ACCESS_KEY_ID;
+
+  private AWS_SECRET_ACCESS_KEY;
+
+  constructor(AWS_ACCESS_KEY_ID: string, AWS_SECRET_ACCESS_KEY: string) {
+    this.AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY_ID;
+    this.AWS_SECRET_ACCESS_KEY = AWS_SECRET_ACCESS_KEY;
   }
 }
